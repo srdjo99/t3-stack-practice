b4186c682fd41075b5823d3d8d03056ce09fbfc7
Srdjan <srdjancoralic5@gmail.com>
2022-11-09 20:02:34 +0100

    add getRandomPokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
-import type { NextPage } from "next";
-import { trpc } from "../utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { useEffect } from "react";
 
-const Home: NextPage = () => {
-  const { data, isLoading } = trpc.hello.useQuery({ text: "Srdjan" });
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
+const Home = () => {
+  const [first, second] = getOptionsForVote();
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="text-2xl text-center ">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="flex items-center justify-between max-w-2xl p-8 border rounded">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
