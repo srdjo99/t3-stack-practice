de37e3893b80ddbc35eda3a2b14506def8dc92df
Srdjan <srdjancoralic5@gmail.com>
2022-11-09 19:33:48 +0100

    add trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+import type { AppType } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.hello.useQuery({ text: "Srdjan" });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (data) return <div>{data.greeting}</div>;
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="text-2xl text-center ">Which Pokemon is Rounder?</div>
